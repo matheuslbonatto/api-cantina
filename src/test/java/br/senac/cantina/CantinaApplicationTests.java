a570400caee01adaa213faa70bc7937833dc8dbf
Matheus Luiz Bonatto <matheuslbonatto@gmail.com>
2024-07-16 20:47:35 -0300

    primeiro commit

---
+++ src/test/java/br/senac/cantina/CantinaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.cantina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CantinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
